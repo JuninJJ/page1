da44c957b0e4225108ecda0fb3dec45efb870da8
Junior Lima <126059420+JuninJJ@users.noreply.github.com>
2024-11-29 14:10:17 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</div>
+    <div>
+      <Navbar />
+    
+    </div>
   )
 }
 
